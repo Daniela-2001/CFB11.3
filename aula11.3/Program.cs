d1220b3150cb98436f981dad4742b7a73a4cf6af
Daniela Duarte <danielatavaresd@gmail.com>
2024-01-19 20:25:32 +0000

    CFB11.3

---
+++ aula11.3/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace aula11._3
+{
+    internal class Program
+    {
+        static void Main(){
+
+            int vInt = 10;
+            short vShort=(short)vInt;
+
+            Console.WriteLine("vShort");
+
+
+        }
+    }
+}
